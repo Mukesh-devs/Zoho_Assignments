c4fbd2ba7f44fd4a68cee5447b63da292eda6d6c
Mukesh-devs <mukesh.cs25@yahoo.com>
2025-05-13 11:09:38 +0530

    Add files via upload

---
+++ Assignment_14/InputMismatchExcept.java
@@ -0,0 +1,16 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class Q3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        try{
+            System.out.print("Enter Your Mobile Number: ");
+            long number = scan.nextLong();
+            System.out.println("Your Mobile Number is: "+ number);
+        }catch(InputMismatchException e){
+            System.out.println("Invalid Number!");
+        }
+
+    }
+}
